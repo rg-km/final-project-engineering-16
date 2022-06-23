74a2edf44250d5faf0b89a0f458ace7698a25da5
renapr <renapril15@gmail.com>
2022-06-23 21:13:11 +0800

    add feature show books and API integration

--- pinjam-buku/src/App.js
+++ pinjam-buku/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Routes, Route, Link } from "react-router-dom";
-import Register from "./account/Register";
-import Home from './user/Home';
+import { Routes, Route, Link } from "react-router-dom"
+import Register from "./account/Register"
+import Home from './user/Home'
 import Login from './account/Login'
+import Galeri from './user/Galeri'
 
 function App() {
   return (
     <div className="App">
-      <Routes>        
+      <Routes>
         <Route index element={<Home />} />
         <Route path="/daftar" element={<Register />} />
         <Route path='/masuk' element={<Login />} />
+        <Route path="/galeri-buku" element={<Galeri />} />
       </Routes>
     </div>
   );
